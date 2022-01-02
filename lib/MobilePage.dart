78814e77a78ce46fe8b5002551d7d31bb1112722
Jcupzz <jcupzz255@gmail.com>
2022-01-02 21:40:11 +0530

    added footer

--- lib/MobilePage.dart
+++ lib/MobilePage.dart
@@ -1222,6 +1222,101 @@ class _MobileViewState extends State<MobileView> {
                 ],
               ),
             ),
+
+            //footer
+
+            Container(
+              color: const Color.fromRGBO(36, 37, 41, 1),
+              padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
+              width: screenWidth,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        //Made with ♥ CODe 2021
+                        //Made with Flutter
+                        children: const [
+                          CircleAvatar(
+                            radius: 12.0,
+                            backgroundImage: AssetImage('assets/images/fb.png'),
+                            backgroundColor: Colors.transparent,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          CircleAvatar(
+                            radius: 12.0,
+                            backgroundImage:
+                                AssetImage('assets/images/twitter.png'),
+                            backgroundColor: Colors.transparent,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          CircleAvatar(
+                            radius: 12.0,
+                            backgroundImage:
+                                AssetImage('assets/images/linkedin.png'),
+                            backgroundColor: Colors.transparent,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          CircleAvatar(
+                            radius: 12.0,
+                            backgroundImage:
+                                AssetImage('assets/images/insta.png'),
+                            backgroundColor: Colors.transparent,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            "  Made with 🤍 CODe 2021\n",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Poppins',
+                                fontSize: 10),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            CircleAvatar(
+                              radius: 7.0,
+                              backgroundImage:
+                                  AssetImage('assets/images/flutter.png'),
+                              backgroundColor: Colors.transparent,
+                            ),
+                            Text(
+                              " Made using Flutter",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Poppins',
+                                  fontSize: 10),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
