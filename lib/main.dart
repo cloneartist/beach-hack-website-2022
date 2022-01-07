0e7a2616a1012e09a15a1a1fae3e010648f594b4
Jcupzz <jcupzz255@gmail.com>
2022-01-08 00:06:05 +0530

    removed test pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:beach_hack_website/Home.dart';
 import 'package:beach_hack_website/HomePage.dart';
 import 'package:beach_hack_website/MobilePage.dart';
-import 'package:beach_hack_website/test.dart';
-import 'package:beach_hack_website/testtyy.dart';
 import 'package:flutter/material.dart';
 
 void main() {
